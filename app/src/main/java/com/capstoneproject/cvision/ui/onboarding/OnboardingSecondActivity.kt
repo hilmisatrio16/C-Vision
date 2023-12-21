104a464e5531112f3c42696c8e0982d789ed0136
muhammadhilmisatrio <hilmisatrio16@gmail.com>
2023-12-21 18:26:11 +0700

    change intent route

--- app/src/main/java/com/capstoneproject/cvision/ui/onboarding/OnboardingSecondActivity.kt
+++ app/src/main/java/com/capstoneproject/cvision/ui/onboarding/OnboardingSecondActivity.kt
@@ -9,6 +9,7 @@ import android.view.View
 import com.capstoneproject.cvision.MainActivity
 import com.capstoneproject.cvision.R
 import com.capstoneproject.cvision.databinding.ActivityOnboardingSecondBinding
+import com.capstoneproject.cvision.ui.login.LoginActivity
 
 class OnboardingSecondActivity : AppCompatActivity() {
 
@@ -21,7 +22,7 @@ class OnboardingSecondActivity : AppCompatActivity() {
 
         setAnimation()
         binding.btnStarted.setOnClickListener {
-            val intent = Intent(this, MainActivity::class.java)
+            val intent = Intent(this, LoginActivity::class.java)
             intent.flags =
                 Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
             startActivity(intent)
