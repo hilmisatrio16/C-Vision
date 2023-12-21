57a95a47d4a324d61a3850fc77dfc8ac8051f204
muhammadhilmisatrio <hilmisatrio16@gmail.com>
2023-12-22 03:53:08 +0700

    get data and show data detail result

--- app/src/main/java/com/capstoneproject/cvision/ui/diagnose/DetailResultFragment.kt
+++ app/src/main/java/com/capstoneproject/cvision/ui/diagnose/DetailResultFragment.kt
@@ -6,16 +6,40 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.capstoneproject.cvision.R
+import com.capstoneproject.cvision.databinding.FragmentDetailResultBinding
 
 
 class DetailResultFragment : Fragment() {
+
+    private lateinit var binding: FragmentDetailResultBinding
+
+    private var result = ""
+    private var classPrediction = ""
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_detail_result, container, false)
+        binding = FragmentDetailResultBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        arguments?.let {
+            result = it.getString("RESULT JENIS").toString()
+            classPrediction = it.getString("RESULT CLASS").toString()
+        }
+
+        binding.tvDiagnose.text = result
+
+        binding.tvDescription.text = if (classPrediction != "0") {
+            resources.getString(R.string.normal_description)
+        } else {
+            resources.getString(R.string.cataract_description)
+        }
+
     }
 
 }
