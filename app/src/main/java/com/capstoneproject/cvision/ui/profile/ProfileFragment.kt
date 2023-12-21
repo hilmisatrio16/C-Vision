33e87642b97d66e7d1d6ede4a98830d86e2aac42
muhammadhilmisatrio <hilmisatrio16@gmail.com>
2023-12-21 18:26:28 +0700

    add viewmodel

--- app/src/main/java/com/capstoneproject/cvision/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/capstoneproject/cvision/ui/profile/ProfileFragment.kt
@@ -7,7 +7,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.viewModels
 import androidx.core.content.ContextCompat
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.capstoneproject.cvision.R
 import com.capstoneproject.cvision.databinding.FragmentProfileBinding
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
@@ -15,6 +18,10 @@ import com.google.android.material.dialog.MaterialAlertDialogBuilder
 class ProfileFragment : Fragment() {
 
     private lateinit var binding: FragmentProfileBinding
+
+    private val profileVM by viewModels<ProfileViewModel>{
+        ProfileViewModelFactory.getInstance(requireActivity())
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -29,6 +36,24 @@ class ProfileFragment : Fragment() {
 
         binding.logout.setOnClickListener {
             showMessageConfirm()
+        }
+
+        binding.credits.setOnClickListener {
+            findNavController().navigate(R.id.action_profileFragment_to_creditsActivity)
+        }
+
+        binding.about.setOnClickListener {
+            findNavController().navigate(R.id.action_profileFragment_to_aboutActivity)
+        }
+
+        showNameUser()
+    }
+
+    private fun showNameUser() {
+        profileVM.getName().observe(viewLifecycleOwner){
+            if(it.isNotEmpty()){
+                binding.tvNameUser.text = it
+            }
         }
     }
 
